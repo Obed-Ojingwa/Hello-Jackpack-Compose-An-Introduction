507c1370ae1d6df5f458c95c4d01fd232be97a4a
Obed <obedojingwa@gmail>
2024-11-11 15:30:46 +0100

    Column Arrangement and alignment

--- app/src/main/java/com/obedcodes/myapplication/MainActivity.kt
+++ app/src/main/java/com/obedcodes/myapplication/MainActivity.kt
@@ -3,22 +3,44 @@ package com.obedcodes.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.obedcodes.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column (modifier = Modifier
+                .background(color = Color.LightGray)
+                .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+                ){
 
-            Greeting("Android")
-
+                Greeting("Android")
+                Greeting("Obed")
+                Greeting("Windows")
+                Greeting("G")
+            }
         }
     }
 }
@@ -27,9 +49,21 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Red,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            .border(2.dp, Color.Green)
+            .padding(10.dp)
+            /*.fillMaxWidth(0.5f)
+            .fillMaxHeight(0.3f)*/
+
     )
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
